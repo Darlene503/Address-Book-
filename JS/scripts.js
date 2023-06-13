2eb334936605bcf0d5f9af7f8779a786b1d4ab2b
Darlene <duyn503@gmail.com>
2023-06-13 11:38:41 -0700

    create address book site and functionality Co-authored by: Jason Church elijahchurch@gmail.com

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,8 +5,6 @@ function AddressBook() {
     this.currentId = 0;
 }
 
-let addressBook = new AddressBook();
-
 AddressBook.prototype.addContact = function(contact) {
     contact.id = this.assignId();
     this.contacts[contact.id] = contact;
@@ -46,3 +44,58 @@ Contact.prototype.update = function(newFirst, newLast, newPhone) {
         this.phoneNumber = newPhone;
     } 
 }
+
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+
+function listContacts(addressBookToDisplay) {
+    let contactsDiv = document.querySelector("div#contacts");
+    contactsDiv.innerText =  null;
+    const ul = document.createElement("ul");
+    Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+        const contact = addressBookToDisplay.findContact(key);
+        const li = document.createElement("li");
+        li.append(contact.fullName());
+        li.setAttribute("id", contact.id);
+        ul.append(li);
+    });
+    contactsDiv.append(ul);
+}
+
+function displayContactDetails(event) {
+    const contact = addressBook.findContact(event.target.id);
+    document.querySelector("span#first-name").innerText = contact.firstName;
+    document.querySelector("span#last-name").innerText = contact.lastName;
+    document.querySelector("span#phone-number").innerText = contact.phoneNumber;
+    document.querySelector("button.delete").setAttribute("id", contact.id);
+    document.querySelector("div#contact-details").removeAttribute("class");
+}
+
+
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  addressBook.addContact(newContact);
+  listContacts(addressBook); 
+  document.querySelector("input#new-first-name").value = null;
+  document.querySelector("input#new-last-name").value = null;
+  document.querySelector("input#new-phone-number").value = null;
+}
+
+function handleDelete(event) {
+    addressBook.deleteContact(event.target.id);
+    document.querySelector("button.delete").removeAttribute("id");
+    document.querySelector("div#contact-details").setAttribute("class", "hidden");
+    listContacts(addressBook);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+  document.querySelector("div#contacts").addEventListener("click", displayContactDetails); 
+  document.querySelector("button.delete").addEventListener("click", handleDelete);
+});
